6ff05d0b0e9878c4500a8de4fe8fad6462d91cae
Mauro <jmaurodev@gmail.com>
2025-04-21 18:43:40 -0300

    Ajuste de enquete_programacao para enquetes

--- enquetes/wsgi.py
+++ enquetes/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enquete_programacao.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enquetes.settings')
 
 application = get_wsgi_application()
