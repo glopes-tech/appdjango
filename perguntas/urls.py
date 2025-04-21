54727c2f29f9e6cb699cd97c6e02bd87ee4ee965
= <=>
2025-04-21 20:06:18 -0300

    urls

--- perguntas/urls.py
+++ perguntas/urls.py
@@ -6,4 +6,13 @@ urlpatterns = [
     path('enquete/<int:enquete_id>/', views.responder_enquete, name='responder_enquete'),
     path('enquete/<int:enquete_id>/respondida/', views.enquete_respondida, name='enquete_respondida'),
     path('enquete/<int:enquete_id>/resultados/', views.exibir_resultados, name='exibir_resultados'),
+
+    path('alunos/', views.listar_alunos, name='listar_alunos'),
+    path('alunos/<int:aluno_id>/', views.detalhar_aluno, name='detalhar_aluno'),
+
+    path('areas-interesse/', views.listar_areas_interesse, name='listar_areas_interesse'),
+    path('areas-interesse/<int:area_id>/', views.detalhar_area_interesse, name='detalhar_area_interesse'),
+
+    path('interesses/', views.listar_interesses_alunos, name='listar_interesses_alunos'),
+    path('interesses/<int:interesse_id>/', views.detalhar_interesse_aluno, name='detalhar_interesse_aluno'),
 ]
