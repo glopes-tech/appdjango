f1b1dc1fcd4888ac87b45c92092cab9817fb8a96
= <=>
2025-05-05 11:07:46 -0300

    Update urls.py

--- perguntas/urls.py
+++ perguntas/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('enquetes/', views.gerenciar_enquetes, name='gerenciar_enquetes'),
     path('usuarios/', views.gerenciar_usuarios, name='gerenciar_usuarios'),
     path('usuarios/registrar/', views.registrar_aluno, name='registrar_aluno'),
-    path('admin/criar-enquete/', views.criar_enquete, name='admin_criar_enquete'),
     path('areas-interesse/', views.listar_areas_interesse, name='listar_areas_interesse'),
     path('enquete/<int:enquete_id>/responder/', views.gerenciar_enquetes, name='responder_enquete'),
     path('enquete/<int:enquete_id>/resultados/', views.gerenciar_enquetes, name='exibir_resultados'),
