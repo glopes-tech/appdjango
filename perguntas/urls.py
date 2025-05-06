5c1745f6a6d0eba8d6624af1cab94c943c3d2560
= <=>
2025-05-06 13:27:50 -0300

    correcao_urls

--- perguntas/urls.py
+++ perguntas/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('enquetes/', views.gerenciar_enquetes, name='gerenciar_enquetes'),
+    path('enquetes/gerenciar/', views.gerenciar_enquetes, name='enquetes/gerenciar_enquetes'),
     path('usuarios/', views.gerenciar_usuarios, name='gerenciar_usuarios'),
     path('usuarios/registrar/', views.registrar_aluno, name='registrar_aluno'),
     path('admin/criar-enquete/', views.criar_enquete, name='admin_criar_enquete'),
